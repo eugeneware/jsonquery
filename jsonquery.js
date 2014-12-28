634f06203c1c04d80545d8331e14d20d2905d365
Mike Bild <mike.bild@googlemail.com>
2014-12-28 19:22:26 +0100

    typeof match fix

--- jsonquery.js
+++ jsonquery.js
@@ -55,7 +55,7 @@
       v = predicate[n];
       if (n[0] === '$') {
         matches += operator(n, v, haystack);
-      } else if (v && v.constructor === Object) {
+      } else if (v && (v.constructor === Object || typeof v === 'object')) {
         if (valOpMatch(lookup(n, haystack), v, haystack)) {
           matches++;
         }
